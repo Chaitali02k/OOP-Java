4d4154923f5bb378d12ebb8edf8d15b95cb55cbd
chaitali kulkarni <ckulkarni289@gmail.com>
2018-05-03 15:25:38 +0200

    Reverse Array list

---
+++ reverseArrayList/src/reverseArrayList/ReverseArrayList.java
@@ -0,0 +1,25 @@
+package reverseArrayList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ReverseArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> numbers = new ArrayList<>();
+
+		numbers.add(3);
+		numbers.add(6);
+		numbers.add(9);
+		numbers.add(12);
+		
+		System.out.println("Element in order:"+numbers);
+		
+		Collections.reverse(numbers);
+		
+		System.out.println("Elements in reverse order"+numbers);
+
+	}
+
+}
